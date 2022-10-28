776d42e401e79cd07b46e794ead9e23e6817bc8e
anshulvishnaliya <116366206+anshulvishnaliya@users.noreply.github.com>
2022-10-28 18:40:28 +0530

    Anshul age calculator code

--- Anshul/Anshul_agecalculator.cs
+++ Anshul/Anshul_agecalculator.cs
@@ -1,15 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace Github_Tutorial.Anshul
 {
-    internal class Anshul_agecalculator
+    public class Age
     {
-        public void func()
+        public int current_date = 27;
+        public int current_month = 10;
+        public int current_year = 2022;              // Query -> Const access in main , Public because it is needed in main
+        int result_date, result_month, result_year;
+
+        public void logic(int date, int month, int year, int month_day, int total_month)
         {
-            Console.WriteLine("Anshul Prajapati");
+            if (current_month <= month && current_date < date)
+            {
+                result_year = current_year - year - 1;
+
+                if (current_month == month)
+                {
+                    result_month = total_month - month + current_month - 1;
+                }
+                else
+                {
+                    result_month = total_month - month + current_month;
+                }
+
+                result_date = month_day - date + current_date;
+
+                Console.WriteLine($"You are {result_year} years {result_month} months {result_date} days old");
+            }
+
+            else if (current_month >= month && current_date > date)
+            {
+                result_year = current_year - year;
+
+                if (current_month == month)
+                {
+                    result_month = 0;
+                }
+                else
+                {
+                    result_month = current_month - month;
+                }
+
+                result_date = current_date - date;
+                Console.WriteLine($"You are {result_year} years {result_month} months {result_date} days old");
+            }
+
+            else if (current_month < month && current_date > date)
+            {
+                result_year = current_year - year - 1;
+                result_month = total_month - month + current_month;
+                result_date = current_date - date;
+                Console.WriteLine($"You are {result_year} years {result_month} months {result_date} days old");
+            }
+
+            else if (current_month > month && current_date < date)
+            {
+                result_year = current_year - year;
+                result_month = current_month - month - 1;
+                result_date = month_day - date + current_date;
+                Console.WriteLine($"You are {result_year} years {result_month} months {result_date} days old");
+            }
+
+            else
+            {
+                result_year = current_year - year;
+                Console.WriteLine($"You are {result_year} years {result_month} months {result_date} days old");
+            }
+
         }
-        
-    }
+    }     
 }
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Github_Tutorial.Anshul;
+using System;
 
 namespace Github_Tutorial
 {
@@ -6,10 +7,95 @@ namespace Github_Tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Nishant Gupta!");
+            int month_day = 0, total_month = 12;
+            Console.Write("Enter your birth date : ");
+            var date = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your birth month : ");
+            var month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your birth year : ");
+            var year = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Anshul Prajapati");
+            var obj = new Age();
 
+            switch (month)
+            {
+                case 1:
+                    month_day = 31;
+                    break;
+                case 2:
+                    if (year % 4 == 0)       // To find a year is leap simply divide by 4 if remainder 0 then it is leap 
+                    {
+                        month_day = 29;
+                    }
+                    else
+                    {
+                        month_day = 28;
+                    }
+                    break;
+                case 3:
+                    month_day = 31;
+                    break;
+                case 4:
+                    month_day = 30;
+                    break;
+                case 5:
+                    month_day = 31;
+                    break;
+                case 6:
+                    month_day = 30;
+                    break;
+                case 7:
+                    month_day = 31;
+                    break;
+                case 8:
+                    month_day = 31;
+                    break;
+                case 9:
+                    month_day = 30;
+                    break;
+                case 10:
+                    month_day = 31;
+                    break;
+                case 11:
+                    month_day = 30;
+                    break;
+                case 12:
+                    month_day = 31;
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid Month number and again restart the program...");
+                    break;
+            }
+
+            // This code is written for if user input greater DOB than current date 
+
+            if (year == obj.current_year && month >= obj.current_month && date > obj.current_date)
+            {
+                Console.WriteLine("Date of Birth must be less than or equal to current date...");
+            }
+            else if(date < obj.current_date && month > obj.current_month && year == obj.current_year)
+            {
+                Console.WriteLine("Date of Birth must be less than or equal to current date...");
+            }
+            else if (year > obj.current_year)                // If user input future year number
+            {
+                Console.WriteLine("Date of Birth must be less than or equal to current date...");
+            }
+            else if(date > month_day)                        // If user input invalid date number
+            {
+                if(month_day == 0)
+                {
+                    // If month_day = 0 then i want to print nothing
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid Date number and again restart the program...");
+                }
+            }
+            else
+            {
+                obj.logic(date, month, year, month_day, total_month);
+            }
         }
     }
 }
